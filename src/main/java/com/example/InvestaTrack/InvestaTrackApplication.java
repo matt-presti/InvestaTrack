dc233fa5803642c56e3d7c010e3d8cc2734dd354
matt-presti <mapresti3@gmail.com>
2025-07-02 13:41:51 -0700

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/InvestaTrack/InvestaTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvestaTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestaTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestaTrackApplication.class, args);
+	}
+
+}
