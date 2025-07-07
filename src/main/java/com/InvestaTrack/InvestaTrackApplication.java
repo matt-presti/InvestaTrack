56389d5cd4388663503b837993626093aa30cda4
matt-presti <mapresti3@gmail.com>
2025-07-07 12:56:43 -0700

    Add project configuration and main application class

--- src/main/java/com/InvestaTrack/InvestaTrackApplication.java
+++ src/main/java/com/InvestaTrack/InvestaTrackApplication.java
@@ -1,4 +1,4 @@
-package com.example.InvestaTrack;
+package com.InvestaTrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
